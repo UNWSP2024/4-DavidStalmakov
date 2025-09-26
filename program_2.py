2fb085b43c205b22b200c0123c8384d1660d9d2e
DavidStalmakov <dmstalmakov@students.unwsp.edu>
2025-09-26 12:35:57 -0500

    Homework, the flowchart will be on a google doc and the pseudocode in attached file.

--- program_2.py
+++ program_2.py
@@ -3,10 +3,20 @@
 # tickets are desired for each movie.  
 # At the end of the program it prints out the total number of tickets desired by the user.  
 # Use either a "for loop" or "while loop" to accomplish this.
-
+# David Stalmakov 9/25/2025 "Number of Tickets for Movies"
 def main():
     ######################
-    # WRITE YOUR CODE HERE
+    total_tickets = 0
+    count = 0
+    number_of_movies = int(input("Enter the number of movies you want to buy tickets for: "))
+
+    while count < number_of_movies:
+        movie = input("Enter the movie title: ")
+        tickets = int(input("Enter the number of tickets: "))
+        total_tickets += tickets
+        count += 1
+
+    print("Total tickets: ", total_tickets)
     ######################
 
 
